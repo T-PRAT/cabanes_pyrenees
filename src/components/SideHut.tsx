fa37cc4ce06a5114d86ef5deb8e64d648f750025
tprat <Titouan.p@hotmail.fr>
2023-11-15 10:33:39 +0100

    add shadcnui lib

--- src/components/SideHut.tsx
+++ src/components/SideHut.tsx
@@ -18,35 +18,31 @@ export default function SideHut() {
       <div className="p-2 my-4 border border-zinc-400">
         <h2 className="text-xl font-semibold text-center">{hut?.name}</h2>
         <p className="py-3 text-md">{hut?.description}</p>
-        <table className="my-2 text-left border table-auto border-zinc-500">
+        <table className="my-2 text-left border table-auto border-">
           <tbody>
             <tr>
-              <th className="p-2 border border-zinc-500">Capacité été:</th>
-              <td className="p-2 border border-zinc-500">
-                {hut?.summer_capacity}
-              </td>
+              <th className="p-2 border border-">Capacité été:</th>
+              <td className="p-2 border border-">{hut?.summer_capacity}</td>
             </tr>
             <tr>
-              <th className="p-2 border border-zinc-500">Capacité hiver:</th>
-              <td className="p-2 border border-zinc-500">
-                {hut?.winter_capacity}
-              </td>
+              <th className="p-2 border border-">Capacité hiver:</th>
+              <td className="p-2 border border-">{hut?.winter_capacity}</td>
             </tr>
           </tbody>
         </table>
-        <table className="text-center border table-auto border-zinc-500">
+        <table className="text-center border table-auto border-">
           <thead>
             <tr>
-              <th className="p-2 border border-zinc-500">Altitude</th>
-              <th className="p-2 border border-zinc-500">Latitude</th>
-              <th className="p-2 border border-zinc-500">Longitude</th>
+              <th className="p-2 border border-">Altitude</th>
+              <th className="p-2 border border-">Latitude</th>
+              <th className="p-2 border border-">Longitude</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td className="p-2 border border-zinc-500">{hut?.altitude}</td>
-              <td className="p-2 border border-zinc-500">{hut?.latitude}</td>
-              <td className="p-2 border border-zinc-500">{hut?.longitude}</td>
+              <td className="p-2 border border-">{hut?.altitude}</td>
+              <td className="p-2 border border-">{hut?.latitude}</td>
+              <td className="p-2 border border-">{hut?.longitude}</td>
             </tr>
           </tbody>
         </table>
--- src/layout/SideBar.tsx
+++ src/layout/SideBar.tsx
@@ -1,12 +1,16 @@
+import { Button } from "@/components/ui/button";
 import SideHut from "../components/SideHut";
 
 export default function SideBar() {
   return (
-    <div className="z-10 flex flex-col w-[400px] h-screen p-4 bg-zinc-200 text-zinc-900 border-r-zinc-600 ring-2 ring-zinc-900/40">
-      <h1 className="py-3 my-3 text-3xl font-bold text-center border-b border-zinc-500">
+    <div className="z-10 w-[400px] h-screen p-4 ring-2 ring-primary/50">
+      <h1 className="bg-gradient text-3xl p-4 !bg-clip-text text-transparent !bg-cover !bg-center leading-none uppercase tracking-tighter font-black">
         Cabane des Pyrénées
       </h1>
       <SideHut />
+      <div className="fixed bottom-4">
+        <Button>Ajouter</Button>
+      </div>
     </div>
   );
 }
